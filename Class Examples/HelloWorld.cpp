3c706760c5ad602ded87e27af35bcfff9719f8d4
Shelby King <shelby.king593@my.tccd.edu>
2019-01-15 17:23:04 -0600

    Add files via upload

---
+++ Class Examples/HelloWorld.cpp
@@ -0,0 +1,21 @@
+//Shelby King
+//Class Example
+//15 January 2019
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+
+	cout << "What is your name? ";
+	cin >> name;
+
+	cout << "Hello, " << name << ". I am your computer!" << endl;
+
+	system("PAUSE");
+	return 0;
+}
