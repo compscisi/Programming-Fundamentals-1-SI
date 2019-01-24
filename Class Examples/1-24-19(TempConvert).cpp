acf51860903c89e355b1ef8ad1998aaa3320ea8b
Shelby King <shelby.king593@my.tccd.edu>
2019-01-24 16:10:40 -0600

    Add files via upload

---
+++ Class Examples/1-24-19(TempConvert).cpp
@@ -0,0 +1,35 @@
+/******************************
+   Mr. Class Example
+   24 January 2019
+   TempConvert.cpp
+   Description: This program will ask for the date and temperature in
+   Fahrenheit and convert it to Celsius
+   NOTE: This example is INCOMPLETE
+********************************/
+
+// Headers
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+// Global Variables
+
+int main()
+{
+	//	Your code goes here
+    
+    
+	//	Make sure we place the end message on a new line
+    cout << endl;
+
+	//	The following is system dependent.  It will only work on Windows
+    system("PAUSE");
+
+	/* 
+	// A non-system dependent method is below
+	cout << "Press any key to continue";
+	cin.get();
+	*/
+    return 0;
+}
