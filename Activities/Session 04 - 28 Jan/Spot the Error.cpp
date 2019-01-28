571f8bfa67075fb0e99f14b93701a0293af91b9e
Shelby King <shelby.king593@my.tccd.edu>
2019-01-28 02:20:34 -0600

    Update Spot the Error.cpp

--- Activities/Session 04 - 28 Jan/Spot the Error.cpp
+++ Activities/Session 04 - 28 Jan/Spot the Error.cpp
@@ -2,9 +2,11 @@
    Mr. Bad Programmer
    28 January 2019
    spotTheError.cpp
-   Description: Use this code to
-	fill out the Spot the Error 
-	worksheet!
+   Description: The following program
+   is supposed to display the results 
+   of a survey about energy drinks.
+   Use this code to fill out the 
+   Spot the Error worksheet!
 ********************************/
 
 // Headers
@@ -22,22 +24,21 @@ int main()
 {
 	//	Your code goes here
  	const int NUM_SURVEYED = 16500;
+	
+	float percent_1_or_more = .15,
+	percent_pref_citrus = .58,
+	total_1_or_more = NUM_SURVEYED * percent_1_or_more,
+	grand_total = total_1_or_more * percent_pref_citrus;
 
-   	  float percent_1_or_more = .15,
-      percent_pref_citrus = .58,
-      total_1_or_more = NUM_SURVEYED * percent_1_or_more,
-      grand_total = total_1_or_more * percent_pref_citrus;
-
- 	   cout << endl << endl;
-   	 cout << "The approximate # of customers in the survey who\n";
-   	 cout << "purchase on or more energy drinks per week is ";
-    	cout << total_1_or_more << endl;
+	cout << endl << endl;
+	cout << "The approximate # of customers in the survey who\n";
+	cout << "purchase on or more energy drinks per week is ";
+	cout << total_1_or_more << endl;
     
-  	 cout << "The approximate # of customers in the survey who\n";
-   	 cout << "prefer citrus-flavored energy drinks ";
-   	 cout << grand_total << endl;
-    	cout << endl << endl;
-    return 0;   
+	cout << "The approximate # of customers in the survey who\n";
+	cout << "prefer citrus-flavored energy drinks ";
+	cout << grand_total << endl;
+
     
 	//	Make sure we place the end message on a new line
     cout << endl;
