6727a2c32dcbac25056b53bc56cc52558725792c
Shelby King <shelby.king593@my.tccd.edu>
2019-01-30 14:04:56 -0600

    Create datatypesizes.cpp

---
+++ Activities/Session 06/datatypesizes.cpp
@@ -0,0 +1,22 @@
+/**************************
+Example to show you how to find the
+sizes of certain datatypes!
+***************************/
+
+#include <iostream>
+#include <cstlib>
+#include <string>
+using namespace std;
+
+int main()
+{
+  //copy and paste the following example to find out the size of various data types
+  cout << "The size of an integer is " << sizeof(int);
+  cout << " bytes.\n";
+
+  //the following is a system dependent that only work with windows; DONT TOUCH THIS
+  SYSTEM("PAUSE");
+  
+  //this marks the end of your program
+  return 0;
+}
