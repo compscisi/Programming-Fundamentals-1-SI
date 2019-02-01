4a0f8fa81b15d1327303d2c9fd4bd31ffb87c1b3
Shelby King <shelby.king593@my.tccd.edu>
2019-02-01 15:59:23 -0600

    Create staticcast.cpp

---
+++ Activities/Session 09/staticcast.cpp
@@ -0,0 +1,17 @@
+/*******************************************************************************
+Complete the following program skeleton so it asks the user to enter a character.
+Store the character in the variable letter. Use a type cast expression with the
+variable in a cout statement to display the character’s ASCII code on the screen.
+********************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  char letter;
+  // Finish this program as specified above
+  
+  system("PAUSE");
+  
+  return 0;
+}
