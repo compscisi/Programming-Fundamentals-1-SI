eb1897290d81c1e08e3416ec1468486b83b12397
Shelby King <shelby.king593@my.tccd.edu>
2019-02-01 16:03:25 -0600

    Update and rename Activities/Session 09/staticcast.cpp to Activities/Session 09 - 5 Feb/staticcast.cpp

--- Activities/Session 09 - 5 Feb/staticcast.cpp
+++ Activities/Session 09 - 5 Feb/staticcast.cpp
@@ -1,4 +1,7 @@
-/*******************************************************************************
+/*******************************************************************************]
+Upload this template file in visual studio. Complete the code by following the 
+directions below. This will help you understand the static cast operator.
+
 Complete the following program skeleton so it asks the user to enter a character.
 Store the character in the variable letter. Use a type cast expression with the
 variable in a cout statement to display the character’s ASCII code on the screen.
