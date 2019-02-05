f4018c4ab7554a909954fbf9dcb02695c7769de4
Shelby King <shelby.king593@my.tccd.edu>
2019-02-05 14:17:04 -0600

    Create mathtemp.cpp

---
+++ Activities/Session 09 - 5 Feb/mathtemp.cpp
@@ -0,0 +1,27 @@
+/*********************************************
+Complete the following program so that volume
+is calculated as pi times r squared time the 
+height ((pi)(h)(r)^(2)). Where pi = 3.14159, r
+is the radius of the tank, and h is the height
+of the tank. You will also need to have the user
+input the radius and the height.
+*********************************************/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double volume, radius, height;
+    
+    cout << "This program will tell you the volume of\n";
+    cout << "a cylinder-shaped fuel tank.\n";
+    cout << "How tall is the tank? ";
+
+    cout << "What is the radius of the tank? ";
+
+    
+    // You must complete the program.
+    
+}
