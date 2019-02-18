746efc58e34a50a7b56ab8f7c2f408cb4fa9c50c
Shelby King <shelby.king593@my.tccd.edu>
2019-02-18 11:15:34 -0600

    Add files via upload

---
+++ Extra Coding Practice/C3 Practice/C3AngleCalc.cpp
@@ -0,0 +1,46 @@
+/******************************
+Programming Challenge: C3 Angle Calculator
+Write a program that asks the user for an angle, entered in radians. The program
+should then display the sine, cosine, and tangent of the angle. (Use the sin , cos , and
+tan library functions to determine these values.) The output should be displayed in
+fixed-point notation, rounded to four decimal places of precision.
+
+EXTRA CHALLENGE: Have the user enter an angle. Then, ask if that angle is in radians. If not,
+convert the angle from degrees to radians. (This requires an if-then statement) 
+180 degrees = pi radians. 
+
+note: I could not find a solution for this one, so I can just look over your code
+and double check it - if need be.
+********************************/
+
+// Headers
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+using namespace std;
+
+// Global variables
+
+// Function declarations
+
+int main()
+{
+	//Your code goes here
+    
+    
+	//	Make sure we place the end message on a new line
+    cout << endl;
+
+	//	The following is system dependent.  It will only work on Windows
+    system("PAUSE");
+
+	/* 
+	// A non-system dependent method is below
+	cout << "Press any key to continue";
+	cin.get();
+	*/
+    return 0;
+}
+
