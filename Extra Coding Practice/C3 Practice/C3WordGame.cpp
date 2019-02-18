593c28f0bd7e79277ebfb7a983a89ad99c1a96f0
Shelby King <shelby.king593@my.tccd.edu>
2019-02-18 11:15:15 -0600

    Update and rename Extra Coding Practice/C3WordGame.cpp to Extra Coding Practice/C3 Practice/C3WordGame.cpp

--- Extra Coding Practice/C3 Practice/C3WordGame.cpp
+++ Extra Coding Practice/C3 Practice/C3WordGame.cpp
@@ -8,13 +8,16 @@ user to enter the following:
 	The name of a college
 	A profession
 	A type of animal
-	A pet’s name
+	A petâ€™s name
 After the user has entered these items, the program should display the following story,
-inserting the user’s input into the appropriate locations:
+inserting the userâ€™s input into the appropriate locations:
 There once was a person named NAME who lived in CITY . At the age of
 AGE , NAME went to college at COLLEGE . NAME graduated and went to work
 as a PROFESSION . Then, NAME adopted a(n) ANIMAL named PETNAME . They
 both lived happily ever after!
+
+note: I do not have a prepared solution for this, so I can look over your code personally
+if you would like it double checked. Just email me the .cpp file!
 ********************************/
 
 // Headers
