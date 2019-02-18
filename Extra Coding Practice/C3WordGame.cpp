af433f7a48269a90ed56e9075622844e27fb4613
Shelby King <shelby.king593@my.tccd.edu>
2019-02-18 11:00:48 -0600

    Add files via upload

---
+++ Extra Coding Practice/C3WordGame.cpp
@@ -0,0 +1,49 @@
+/******************************
+Programming Challenge: C3 Word Game
+Write a program that plays a word game with the user. The program should ask the
+user to enter the following:
+	His or her name
+	His or her age
+	The name of a city
+	The name of a college
+	A profession
+	A type of animal
+	A pet’s name
+After the user has entered these items, the program should display the following story,
+inserting the user’s input into the appropriate locations:
+There once was a person named NAME who lived in CITY . At the age of
+AGE , NAME went to college at COLLEGE . NAME graduated and went to work
+as a PROFESSION . Then, NAME adopted a(n) ANIMAL named PETNAME . They
+both lived happily ever after!
+********************************/
+
+// Headers
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+// Global variables
+
+// Function declarations
+
+int main()
+{
+	//	Your code goes here
+    
+    
+	//	Make sure we place the end message on a new line
+    cout << endl;
+
+	//	The following is system dependent.  It will only work on Windows
+    system("PAUSE");
+
+	/* 
+	// A non-system dependent method is below
+	cout << "Press any key to continue";
+	cin.get();
+	*/
+    return 0;
+}
+
