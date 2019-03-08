d3a1c6a042968be628539c5631e1139acb07f916
Shelby King <shelby.king593@my.tccd.edu>
2019-03-08 12:12:03 -0600

    Create RomanNumeralConverter.cpp

---
+++ Extra Coding Practice/C4 Practice/RomanNumeralConverter.cpp
@@ -0,0 +1,26 @@
+/****************************************************************************************************************
+Roman Numeral Converter
+Write a program that asks the user to enter a number within the range of 1 through
+10. Use a switch statement to display the Roman numeral version of that number.
+Input Validation: Do not accept a number less than 1 or greater than 10.
+********************************************************************************************************************/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Function prototype
+
+int main()
+{
+	//variables
+
+	//get number from user
+
+	//convert to roman numeral
+
+	//display result
+
+	cout << endl;
+	system("PAUSE");
+	return 0;
+}
