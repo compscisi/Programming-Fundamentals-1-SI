29bebf9567c9533016950724f818bc823ef4d99d
Shelby King <shelby.king593@my.tccd.edu>
2019-03-08 14:23:48 -0600

    Create ColorMixer.cpp

---
+++ Extra Coding Practice/C4 Practice/ColorMixer.cpp
@@ -0,0 +1,32 @@
+/****************************************************************************************************************
+Color Mixer
+The colors red, blue, and yellow are known as the primary colors because they cannot
+be made by mixing other colors. When you mix two primary colors, you get a secondary
+color, as shown here:
+When you mix red and blue, you get purple.
+When you mix red and yellow, you get orange.
+When you mix blue and yellow, you get green.
+Write a program that prompts the user to enter the names of two primary colors to
+mix. If the user enters anything other than “red,” “blue,” or “yellow,” the program
+should display an error message. Otherwise, the program should display the name of
+the secondary color that results by mixing two primary colors.
+********************************************************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+// Function prototype
+
+int main()
+{
+	//variables
+
+	//prompt user to enter primary color
+
+	//compare and display output
+
+	cout << endl << endl;
+	system("PAUSE");
+	return 0;
+}
