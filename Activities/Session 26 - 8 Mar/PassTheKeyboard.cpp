19d363d4f24064c33da1fe49fae20920884a032b
Shelby King <shelby.king593@my.tccd.edu>
2019-03-08 14:27:03 -0600

    Update PassTheKeyboard.cpp

--- Activities/Session 26 - 8 Mar/PassTheKeyboard.cpp
+++ Activities/Session 26 - 8 Mar/PassTheKeyboard.cpp
@@ -1,19 +1,9 @@
 /****************************************************************************************************************
-Color Mixer
+Areas of Rectangles
 
-The colors red, blue, and yellow are known as the primary colors because they cannot
-be made by mixing other colors. When you mix two primary colors, you get a secondary
-color, as shown here:
-
-When you mix red and blue, you get purple.
-When you mix red and yellow, you get orange.
-When you mix blue and yellow, you get green.
-
-Write a program that prompts the user to enter the names of two primary colors to
-mix. If the user enters anything other than “red,” “blue,” or “yellow,” the program
-should display an error message. Otherwise, the program should display the name of
-the secondary color that results by mixing two primary colors.
-
+The area of a rectangle is the rectangle’s length times its width. Write a program that
+asks for the length and width of two rectangles. The program should tell the user which
+rectangle has the greater area, or if the areas are the same.
 ********************************************************************************************************************/
 #include <iostream>
 #include <iomanip>
@@ -25,12 +15,12 @@ using namespace std;
 int main()
 {
 	//variables
-
-	//prompt user to enter primary color
-
-	//compare and display output
-
-	cout << endl << endl;
+	
+	//get user to input values
+	
+	//compare and display result
+	
+	cout << endl;
 	system("PAUSE");
 	return 0;
 }
