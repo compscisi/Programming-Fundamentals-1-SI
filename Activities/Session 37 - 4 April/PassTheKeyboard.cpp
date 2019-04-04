ac2277a9f2f06d7b5f356a6a53e8d9f7c57e63db
Shelby King <shelby.king593@my.tccd.edu>
2019-04-04 08:20:46 -0500

    Create PassTheKeyboard.cpp

---
+++ Activities/Session 37 - 4 April/PassTheKeyboard.cpp
@@ -0,0 +1,33 @@
+/**************************************************************
+PASS THE KEYBOARD
+Using the template below, write a program that asks
+the user to enter the name of file and then displays
+the contents of the file. There are a few files text files that
+need to be included in the project folder for this program:
+	- FileOne.txt
+	- FileTwo.txt
+	- FileThree.txt
+THE CATCH IS: One person may write a section of code at a time.
+***************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>			//so we can use files
+#include <cctype>			//for toupper() method
+using namespace std;
+
+// Function prototype
+void displayFile(string fileName);		//function to display file
+
+int main()
+{
+	//variables
+				
+
+	//loop to get user input and display file
+	
+
+	cout << endl << endl;
+	system("PAUSE");
+	return 0;
+}
