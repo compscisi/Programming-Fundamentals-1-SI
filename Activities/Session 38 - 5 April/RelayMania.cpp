61aed04126c065526a769c20d431543d00170334
Shelby King <shelby.king593@my.tccd.edu>
2019-04-05 14:52:32 -0500

    Create RelayMania.cpp

---
+++ Activities/Session 38 - 5 April/RelayMania.cpp
@@ -0,0 +1,115 @@
+/**************************************************************
+Relay Mania
+Write a menu driven program that displays the contents of a file!
+***************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>			//so we can use files
+#include <cctype>			//for toupper() method
+using namespace std;
+
+// Global constants
+const int MAX_SIZE = 80;	//max size of array
+
+// Function prototype
+int loadFile(string fileStuff[]);			//asks user for file to load then loads into 
+											//array of strings
+void displayFile(string lines[], int size);	//displays the file that is loaded
+
+int main()
+{
+	//variables
+	int choice;						//the users choice
+	string fileContents[MAX_SIZE];	//the files contents with max lines to load set at 80
+	int size;						//size of array
+
+	//using a do-while loop so user can view multiple files
+	do
+	{
+		//display menu and get input from user
+		cout << "-------- FILE VIEWER --------\n";
+		cout << "\t1. Load a File\n"
+			<< "\t2. View the Loaded File\n"
+			<< "\t3. Exit Program\n";
+		cout << "\nEnter a choice from the menu above: ";
+		cin >> choice;
+
+		while (getchar() != '\n');	//flush
+
+		//process the users choice
+		switch (choice)
+		{
+		case 1:	//load the file into an array of strings
+			size = loadFile(fileContents);
+			break;
+		case 2:	//view the file currently loaded
+			displayFile(fileContents, size);
+			break;
+		case 3:	//quit program
+			break;
+		default:
+			cout << "Invalid choice! Please select a choice from the menu above.";
+		}
+		//if we arent quitting the program, flush the screen before looping again
+		if (choice != 3)
+		{
+			cout << endl;
+			system("PAUSE");
+			system("CLS");
+		}
+	} while (choice != 3);
+	
+
+	cout << endl << endl;
+	system("PAUSE");
+	return 0;
+}
+
+int loadFile(string fileStuff[])
+{
+	//clear the current array
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+		fileStuff[i].clear();
+	}
+	//variables
+	string fileName;		//name of the file
+	string fileContent;		//what's in the file
+	int count = 1;			//how many lines read in
+
+	//ask user for file name
+	cout << "\nEnter the name of the file you would like to load: ";
+	getline(cin, fileName);
+
+
+	//open file
+	ifstream inFile;
+	inFile.open(fileName);
+
+	if (!inFile)
+		cout << "File did not load correctly. Please try again";
+	else
+		cout << "File loaded successfully!";
+
+	//use file
+			//for loop to get size of array
+	for (int i = 0; getline(inFile, fileContent); i++, count++)
+	{
+		fileStuff[i] = fileContent;
+	}
+	//CLOSE the file!!!
+	inFile.close();
+	//return size of array
+	return count;
+}
+
+void displayFile(string lines[], int size)
+{
+	cout << "----- FILE CONTENTS -----\n";
+	//display contents of array
+	for (int i = 0; i < size; i++)
+	{
+		cout << lines[i] << endl;
+	}
+}
