deeec1c91f7e73b0dee987c6abea1a830b3f53fa
Shelby King <shelby.king593@my.tccd.edu>
2019-04-07 23:26:35 -0500

    Create ProgrammingChallenge.cpp

---
+++ Activities/Session 39 - 8 April/ProgrammingChallenge.cpp
@@ -0,0 +1,50 @@
+// This program lets the user enter a filename name ListOfNumbers
+// then display all of the values in the
+#include <iostream>
+// What goes Here?
+// What goes Here?
+
+using namespace std;
+
+// Function Declarations
+void pause();
+
+int main()
+{
+    // Create input stream object for input
+    // Create a string for user entered filename
+    // Store input from file into floating point number.
+
+    // Get the filename from the user. Remember to use getline.
+
+
+
+    // Open the file.
+
+
+    // If the file successfully opened, process it.
+    if ()
+    {
+        // Read the numbers from the file and
+        // display them.
+
+
+        // Close the file.
+
+    }
+    else
+    {
+        // Display an error message.
+        
+    }
+
+    pause();
+    return 0;
+}
+
+// Pauses the console
+void pause()
+{
+	cout << "\n\nPress any key to continue....";
+	cin.ignore().get();
+}
