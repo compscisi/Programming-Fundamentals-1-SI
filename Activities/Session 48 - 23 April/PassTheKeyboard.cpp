b127c36426e265f7366da8a5d03eb96eb0b4bce6
Shelby King <shelby.king593@my.tccd.edu>
2019-04-23 13:32:28 -0500

    Create PassTheKeyboard.cpp

---
+++ Activities/Session 48 - 23 April/PassTheKeyboard.cpp
@@ -0,0 +1,39 @@
+//The following program skeleton, when complete, asks the user to enter these data
+//about his or her favorite movie :
+//	Name of movie
+//	Name of the movie’s director
+//	Name of the movie’s producer
+//	The year the movie was released
+//Complete the program by declaring the structure that holds this data, defining a
+//structure variable, and writing the individual statements necessary.
+
+#include <iostream>
+using namespace std;
+// Write the structure declaration here to hold the movie data.
+
+int main()
+{
+	// define the structure variable here.
+	cout << "Enter the following data about your\n";
+	cout << "favorite movie.\n";
+	cout << "name: ";
+	// Write a statement here that lets the user enter the
+	// name of a favorite movie. Store the name in the
+	// structure variable.
+	cout << "Director: ";
+	// Write a statement here that lets the user enter the
+	// name of the movie's director. Store the name in the
+	// structure variable.
+	cout << "Producer: ";
+	// Write a statement here that lets the user enter the
+	// name of the movie's producer. Store the name in the
+	// structure variable.
+	cout << "Year of release: ";
+	// Write a statement here that lets the user enter the
+	// year the movie was released. Store the year in the
+	// structure variable.
+	cout << "Here is data on your favorite movie:\n";
+	// Write statements here that display the data.
+	// just entered into the structure variable.
+	return 0;
+}
